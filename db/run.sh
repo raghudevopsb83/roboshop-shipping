05698ce36750cebcb243db1e0620ecd05c8b271a
Raghu K <raghuk.vit@gmail.com>
2025-04-02 07:54:50 +0530

    RoboShop Code

--- db/run.sh
+++ db/run.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/raghudevopsb83/roboshop-shipping .
 for i in schema app-user master-data ; do
-  mysql -h ${DB_HOST} -u${DB_USER} -p${DB_PASS} < ./db/$i.sql
+  mysql -h ${DB_HOST} -u${DB_USER} -p${DB_PASS} < $i.sql
 done

216cfc301052c6a04dc294432abc6e526670f0b7
sriharshakalluru <raghuk.vit@gmail.com>
2025-05-20 21:48:16 +0530

    New spring boot version and correction as per the new version

--- src/main/java/com/instana/robotshop/shipping/Code.java
+++ src/main/java/com/instana/robotshop/shipping/Code.java
@@ -1,10 +1,10 @@
 package com.instana.robotshop.shipping;
 
-import javax.persistence.Table;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 /*
 * Bean for Code
